14c76b777ab3a0dbaabce104842fb5bd6736d483
meweryk <iliya.pavlov1109@gmail.com>
2026-03-30 19:22:41 +0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v4.19'; // збільште при зміні статичних файлів!
+const CACHE_VERSION = 'v4.20'; // збільште при зміні статичних файлів!
 const STATIC_CACHE_NAME = `atlas-static-${CACHE_VERSION}`;
 const IMAGES_CACHE_NAME = `atlas-images-${CACHE_VERSION}`;
 const DATA_CACHE_NAME = `atlas-data-${CACHE_VERSION}`;
